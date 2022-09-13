ce7e9d47924fa65c7f9bea3da96f9e4b3d79bf16
leogorman123 <leogorman123@gmail.com>
2022-09-13 09:48:05 +0000

    Updated stats-per-admin.js (added 112 lines, deleted 84 lines).

--- stats-per-admin.js
+++ stats-per-admin.js
@@ -22,24 +22,120 @@ we need to make a nested function.
  */
  
 // Outer function with arguments
-var point_count_per_feature = function(
-  imageCollection,
-  band,
-  dateStart,
-  dateEnd,
-  scale,
-  maxPixels
-  ){
-  // Inner function which takes the feature
-  var feature_collection_mapping = function (feature){
+// var point_count_per_feature = function(
+//   imageCollection,
+//   band,
+//   dateStart,
+//   dateEnd,
+//   scale,
+//   maxPixels
+//   ){
+//   // Inner function which takes the feature
+//   var feature_collection_mapping = function (feature){
     
-    var image_collection = imageCollection.
+//     var image_collection = imageCollection.
+//                         // Select the band
+//                         select(band).
+//                         // Filter Date
+//                         filterDate(dateStart, dateEnd)
+                        
+//     var image = image_collection.mode()
+//     // var geometry = feature.geometry()
+
+//     // Get the modal band counts for that region
+//     var pixel_frequency = image.reduceRegion({
+//             reducer:ee.Reducer.frequencyHistogram(),
+//             geometry:geometry,
+//             scale:30,
+//             maxPixels:40e9
+//         }).getInfo()
+        
+//     // Get the total pixel counts for that region
+//     var pixel_count = image.reduceRegion({
+//             reducer:ee.Reducer.count(),
+//             geometry:geometry,
+//             scale:30,
+//             maxPixels:40e9
+//         }).getInfo()
+        
+//     var keys = ee.Dictionary(pixel_frequency[band]).keys()
+//     var values =  ee.Dictionary(pixel_frequency[band]).values()
+//     var indexes = ee.List.sequence(0, ee.Number(keys.length()).subtract(1), 1)
+    
+    
+//     var pixel_ratios = indexes.map(function(index){
+//       var value = ee.Number(values.get(index))
+//       var count = ee.Number(pixel_count[band])
+//       var ratio = ee.Number(value).divide(count)
+//       return(ratio)
+//     })
+
+    
+    
+//     // var adm0_code = feature.get('ADM0_CODE')
+//     var feature_information = feature.toDictionary()
+//     // var geometry = feature.geometry();
+    
+//     // Add new information to the dictionary
+//     feature_information = feature_information.set('dateStart', dateStart)
+//     feature_information = feature_information.set('dateEnd', dateEnd)
+//     feature_information = feature_information.set('scale', scale)
+//     feature_information = feature_information.set('maxPixels', maxPixels)
+//     feature_information = feature_information.set('pixelRatios', pixel_ratios)
+    
+//     var featureCollection = indexes.map(function(index){
+//       var new_dictionary = feature_information
+//       new_dictionary = new_dictionary.set('variable', 'land_cover_class')
+//       // new_dictionary = new_dictionary.set('band', keys.get(index))
+//       // new_dictionary = new_dictionary.set('value', pixel_ratios.get(index))
+      
+//       var subfeature = ee.Feature(
+//       feature.geometry(),
+//       new_dictionary
+//       )
+      
+//       return(subfeature)
+
+      
+//     })
+
+//     var feature_collection = ee.FeatureCollection(
+//       featureCollection
+//       )
+    
+//     return(feature_collection)
+    
+    
+//   }
+//   return(feature_collection_mapping)
+// }
+
+// var mapped_feature = fao_level_1.map(point_count_per_feature(
+//   ee.ImageCollection('MODIS/006/MCD12Q1'),//imageCollection
+//   "LC_Type1",//band
+//   '2018-12-31',//dateStart
+//   '2020-01-02',//dateEnd
+//   30,//scale
+//   40e9//maxPixels
+//   ))
+
+
+var imageCollection= ee.ImageCollection('MODIS/006/MCD12Q1')//imageCollection
+var band = "LC_Type1"//band
+var dateStart = '2018-12-31'//dateStart
+var dateEnd = '2020-01-02'//dateEnd
+var scale = 500 //scale
+var maxPixels = 40e9 //maxPixels
+
+
+var image_collection = imageCollection.
                         // Select the band
                         select(band).
                         // Filter Date
                         filterDate(dateStart, dateEnd)
                         
     var image = image_collection.mode()
+    // var geometry = feature.geometry()
 
     // Get the modal band counts for that region
     var pixel_frequency = image.reduceRegion({
@@ -68,79 +164,11 @@ var point_count_per_feature = function(
       var ratio = ee.Number(value).divide(count)
       return(ratio)
     })
-
     
-    
-    // var adm0_code = feature.get('ADM0_CODE')
-    var feature_information = feature.toDictionary()
-    
-    
-    // How to add new information
-    feature_information = feature_information.set('dateStart', dateStart)
-    feature_information = feature_information.set('dateEnd', dateEnd)
-    feature_information = feature_information.set('scale', scale)
-    feature_information = feature_information.set('maxPixels', maxPixels)
-    feature_information = feature_information.set('pixelRatios', pixel_ratios)
-    
-    var featureCollection = indexes.map(function(index){
-      var new_dictionary = feature_information
-      new_dictionary = new_dictionary.set('variable', 'land_cover_class')
-      new_dictionary = new_dictionary.set('band', keys.get(index))
-      new_dictionary = new_dictionary.set('value', pixel_ratios.get(index))
-      
-      var subfeature = ee.Feature(
-      feature.geometry(),
-      new_dictionary
-      )
-      
-      return(subfeature)
-
-      
-    })
-
-
-    
-    
-    
-    var result = ee.FeatureCollection(
-      featureCollection
-      )
-    
-    return(result)
-    
-    
-  }
-  return(feature_collection_mapping)
-}
-
-var mapped_feature = fao_level_1.map(point_count_per_feature(
-  ee.ImageCollection('MODIS/006/MCD12Q1'),//imageCollection
-  "LC_Type1",//band
-  '2018-12-31',//dateStart
-  '2020-01-02',//dateEnd
-  30,//scale
-  40e9//maxPixels
-  ))
-print("Printing mapped feature")
-
-print(mapped_feature)
+   print(pixel_ratios)
 
 
 
-
-// Categorical Image Collections
-
-var polygon_categories_hist_single_geometry = function(
-  polygon,
-  imageCollection,
-  band,
-  dateStart,
-  dateEnd,
-  scale,
-  maxPixels
-  ){
-  }
-  
 
 
 
