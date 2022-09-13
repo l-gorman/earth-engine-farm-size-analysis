3e430753b4ef874366673dab6c2897f82eb7e7a3
leogorman123 <leogorman123@gmail.com>
2022-09-13 14:28:07 +0000

    Updated continuous-stats-per-admin.js (added 12 lines, deleted 7 lines).

--- continuous-stats-per-admin.js
+++ continuous-stats-per-admin.js
@@ -10,13 +10,16 @@ engine datasets of interest.
 
 
 // Getting Administrative Data -----------------------------------------------
-// var fao_level_1 = ee.FeatureCollection("FAO/GAUL/2015/level1"); 
+var fao_level_1 = ee.FeatureCollection("FAO/GAUL/2015/level1"); 
 // var fao_level_2 = ee.FeatureCollection("FAO/GAUL/2015/level2");
-var fao_level_0 = ee.FeatureCollection("FAO/GAUL/2015/level0");
+// var fao_level_0 = ee.FeatureCollection("FAO/GAUL/2015/level0");
 
 // var filter = ee.Filter.eq('ADM1_CODE', 1927); //2579
 // var filter = ee.Filter.inList('ADM0_CODE', [ 257]) // Admin codes
 // var filter = ee.Filter.inList('ADM0_CODE', [42,79, 94, 133, 155, 182, 205, 257, 253]) // Admin codes
+
+var filter = ee.Filter.inList('ADM0_CODE', [ 85, 177, 255,42, 133]) // Admin codes
+var fao_level_1 = fao_level_1.filter(filter);
 
 
 // var fao_level_1 = fao_level_1.filter(filter);
@@ -32,12 +35,13 @@ var dataset = dataset.mean().multiply(0.02);
 
 var stats_per_region =function(
   dataset,
-  band
+  band,
+  reducer
   ){
     var stats_per_feature = function(feature){
       
       var reduced_region = dataset.reduceRegion({
-              reducer:ee.Reducer.mean(),
+              reducer:reducer,
               geometry:feature.geometry(),
               scale:20000,
               maxPixels:40e9
@@ -57,9 +61,10 @@ var stats_per_region =function(
   }
   
   
-var regional_land_surface_temp = fao_level_0.map(stats_per_region(
+var regional_land_surface_temp = fao_level_1.map(stats_per_region(
   dataset,
-  'LST_AVE'
+  'LST_AVE',
+  ee.Reducer.mean()
   ))
   
 print(regional_land_surface_temp)
@@ -89,6 +94,6 @@ var visualization = {
   ]
 };
 
-Map.setCenter(128.45, 33.33, 5);
+Map.setCenter(25, 0, 3);
 
 Map.addLayer(landAreaImg, visualization, "Land Surface Temperature");
