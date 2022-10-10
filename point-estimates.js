01d439090cca71c9eedc62ea2ee0a3607c66c08a
leogorman123 <leogorman123@gmail.com>
2022-10-10 14:47:14 +0000

    Updated point-estimates.js (added 8 lines).

--- point-estimates.js
+++ point-estimates.js
@@ -3,7 +3,15 @@ var table = ee.FeatureCollection("projects/lgorman/assets/farm-size-all-points")
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // Getting point estimates
 
+
+// Farm Size Points from LSMS
 var farm_size_points = ee.FeatureCollection("projects/lgorman/assets/farm-size-all-points");
+
+// Administrative Units in LSMS
+var fao_level_1 = ee.FeatureCollection("FAO/GAUL/2015/level1"); 
+var fao_level_2 = ee.FeatureCollection("FAO/GAUL/2015/level2");
+
+
 
 print(farm_size_points[1])
 
