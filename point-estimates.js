e894b559a98347b4c3494af6dd2d12ca4262d74e
leogorman123 <leogorman123@gmail.com>
2022-10-10 14:44:09 +0000

    Updated point-estimates.js (added 9 lines, deleted 1 line).

--- point-estimates.js
+++ point-estimates.js
@@ -1,1 +1,9 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var table = ee.FeatureCollection("projects/lgorman/assets/farm-size-all-points");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 // Getting point estimates
+
+var farm_size_points = ee.FeatureCollection("projects/lgorman/assets/farm-size-all-points");
+
+Map.adLayer( farm_size_points,  {'color': 'black'},
+             'Geometry [black]: point'))
